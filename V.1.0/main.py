52c6da2fb86c2e7c0c0260a56413453d8ac49ec2
nbp98 <neelbp.98@gmail.com>
2019-12-02 19:31:25 -0600

    YOLOv3 Model with custom trainign

--- V.1.0/main.py
+++ V.1.0/main.py
@@ -4,9 +4,9 @@
 # DATE CREATED: NOV 2019
 ########################################################################
 
-# Usage example:  python main.py --list=grocery.txt --video=test.mp4     <--VIDEO
-#                 python main.py --list=grocery.txt --image=test.jpg     <--IMAGE
-#                 python main.py --list=grocery.txt                      <--WEBCAM
+# Usage example:  python3 main.py --list=grocery.txt --video=test.mp4     <--VIDEO
+#                 python3 main.py --list=grocery.txt --image=test.jpg     <--IMAGE
+#                 python3 main.py --list=grocery.txt                      <--WEBCAM
 
 
 # Import computer vision package
